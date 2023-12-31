786a501b157ce6e195745b466e4e6e9e90e64e4d
Mehmet Keçeci <71701959+mehmet-k@users.noreply.github.com>
2023-12-31 19:53:47 +0300

    db backup added, to run on local

--- PetSetApp/src/main/java/backend/models/userPurchase.java
+++ PetSetApp/src/main/java/backend/models/userPurchase.java
@@ -1,5 +1,30 @@
 package backend.models;
 
-public class userPurchase {
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user_purchases")
+public class userPurchase {
+	
+	@Id
+	@Column(name = "itemid")
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private int itemID;
+	
+	@Column(name = "userid")
+	private int userID;
+	
+	@Column(name = "status")
+	private int status;
+	
+	@Column(name = "purchasedate")
+	private Date purchaseDate;
 }
