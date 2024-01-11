23c67cf5587839e6242113dae19e4f108cb7d91a
Emir Çağrı Aykın <emircagriaykin@gmail.com>
2024-01-11 18:38:04 +0300

    .

--- PetSetApp/src/main/java/frontend/ManageApplications.java
+++ PetSetApp/src/main/java/frontend/ManageApplications.java
@@ -56,6 +56,11 @@ public class ManageApplications extends JFrame {
         listModel = new DefaultListModel<>();
         petList = new JList<>(listModel);
         listModel.clear();
+        
+
+        updatePetList(pets);
+      
+/*
 
         for (Pet pet : userOwnershipRepository.getUserPets(userr)) {
             if (pet != null) {
@@ -88,6 +93,14 @@ public class ManageApplications extends JFrame {
                 System.out.println("Pet is null");
             }
         }
+        */
+        
+        gbc.gridx = 0;
+        gbc.gridy++;
+        JScrollPane scrollPane = new JScrollPane(petList);
+        
+        panel.add(scrollPane, gbc);
+        
         
         gbc.gridx = 0;
         gbc.gridy++;
@@ -149,8 +162,58 @@ public class ManageApplications extends JFrame {
         listModel.clear();
 
         // Add pet names to the list model
-        for (Pet pet : pets) {
-            listModel.addElement("ID:"+pet.getId()+ "   " + "Name:"+pet.getPetName());
+
+   
+    
+        for (Pet pet : userOwnershipRepository.getUserPets(userr)) {
+            if (pet != null) {
+                try {
+                    List<User> applicants = AdServices.getAllApplicantsByPet(pet);
+                    if (applicants != null) {
+                        for (User uuser : applicants) {
+                            if (uuser != null) {
+                                listModel.addElement("Pet ID:" + pet.getId() + "   " + " Pet Name:" + pet.getPetName()
+                                        + "Applicant ID:"  + "   " + " Applicant Name:"
+                                        + uuser.getFirstName() + " " + uuser.getSurname() + "   "
+                                        + " Applicant Address:" + uuser.getAddress());
+                            } else {
+                                // Handle the case where uuser is null
+                                System.out.println("User is null for pet: " + pet.getId());
+                            }
+                        }
+                    } else {
+                        // Handle the case where applicants is null
+                        System.out.println("Applicants are null for pet: " + pet.getId());
+                    }
+                } catch (Exception e) {
+                    // Handle the exception
+                    System.out.println("An error occurred for pet: " + pet.getId());
+                    e.printStackTrace();
+                }
+        
+            } else {
+                // Handle the case where pet is null
+                System.out.println("Pet is null");
+            }
         }
+    
+    
+    
+    
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
